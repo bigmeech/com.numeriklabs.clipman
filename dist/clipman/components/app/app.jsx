8beaef850b06d2ff513e5ca683e399e65dc7fc78
Larry Eliemenye <larry.eliemenye@tuitravel.com>
2016-12-27 21:56:22 +0100

    release for version 0.0.1

--- dist/clipman/components/app/app.jsx
+++ dist/clipman/components/app/app.jsx
@@ -2,7 +2,7 @@
 
 import React from 'React';
 
-class App extends React.Component {
+export default class App extends React.Component {
   render(){
     return <div>This is a sample component</div>
   }
